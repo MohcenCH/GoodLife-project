27e3bb1f0b8f643a4238fc5e40c95fae607d5cd8
MohcenCH <mohcench@gmail.com>
2020-04-13 22:12:57 +0100

    thefirst commit

---
+++ GoodLife/Unnamed/Git-recap.py
@@ -0,0 +1,14 @@
+$ git config --global user.name MohcenCH
+$ git config --global user.email mohcench@gmail.com    # to link your github account with github
+
+$ git init   # to initialize a file it adds a .git hidden file to track the changes
+$ git branch <branch-name> # to create a branch
+$ git checkout <branch-name> # to switch to the branch that u want to work on 
+$ git checkout -b <branch-name> # to create a branch and switch to it (normalment)
+$ git merge <branch-name> # to merge the branch to the master branch
+$ git diff # to see the last change 
+$ git commit -m "<message here>"
+$ git add 
+$ git push # to upload the files to github 
+$ git status # to see in which branch u r
+$ git pull # to pullout all the changes from the remote repo on github
