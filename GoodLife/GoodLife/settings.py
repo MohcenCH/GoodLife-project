8fc13377c74d301e0e05f6f3dcd0d26428b56d36
MohcenCH <49692542+MohcenCH@users.noreply.github.com>
2022-07-19 20:52:55 +0100

    Update settings.py

--- GoodLife/GoodLife/settings.py
+++ GoodLife/GoodLife/settings.py
@@ -135,8 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 #SMTP Configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = MY_EMAIL_HOST
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mohcench99@gmail.com'
-EMAIL_HOST_PASSWORD = 'bjayafr155'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
